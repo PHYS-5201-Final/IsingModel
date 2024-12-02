4ed16940b709335754bce344af76984637e29fad
slehrman <savlehr@gmail.com>
2024-12-02 00:29:07 -0600

    added stuff

--- Ising_model_2d.py
+++ Ising_model_2d.py
@@ -10,7 +10,7 @@ def calculate_energy(spins, K, h, L):
     for i in range(L):
         for j in range(L):
             energy -= h*spins[i, j]
-            energy -= K*(spins[(i+1)%L, j] + spins[i, (j+1)%L])
+            energy -= K*(spins[(i+1)%L, j] + spins[i, (j+1)%L])*spins[i,j]
     return energy
 
 def metropolis_algorithim(spins, beta, K, h, L):
@@ -23,33 +23,42 @@ def metropolis_algorithim(spins, beta, K, h, L):
 
     #calculating old energy
     old_energy = -h*spins[i, j] - K*(spins[(i+1)%L, j] + spins[i, (j+1)%L]+
-                                     spins[(i - 1) % L, j] +  spins[i, (j-1)%L])
+                                     spins[(i - 1) % L, j] +  spins[i, (j-1)%L])*spins[i, j]
     #calculating new energy
     new_energy = -h*trial_flip_spins[i, j] - K*(trial_flip_spins[(i+1)%L, j] + trial_flip_spins[i, (j+1)%L]+
-                                     trial_flip_spins[(i - 1) % L, j] +  trial_flip_spins[i, (j-1)%L])
+                                     trial_flip_spins[(i - 1) % L, j] +  trial_flip_spins[i, (j-1)%L])*trial_flip_spins[i, j]
     #computing delta E and deciding if we should accept the change
     #new_energy = calculate_energy(trial_flip_spins, K, h, L)
     #old_energy = calculate_energy(spins, K, h, L)
 
     delta_energy = new_energy - old_energy
-    if delta_energy < 0:
+    if delta_energy <= 0:
         spins[i, j] = trial_flip
-        #print("For delta E = {}, the flip at site [{}, {}] was accepted".format(delta_energy, i, j))
-    else:
+       # print("For delta E = {}, the flip at site [{}, {}] was accepted".format(delta_energy, i, j))
+    elif delta_energy > 0:
         w = np.exp(-beta*delta_energy)
         r = random.random()
         if r <= w:
            # print("Random number {}, For delta E = {}".format(r, delta_energy))
             spins[i,j] = trial_flip
+
     return(spins)
 
 def monte_carlo(beta, K, h, L, steps, spins):
     energies = [calculate_energy(spins, K, h, L)]
+    energies_squared = [(calculate_energy(spins, K, h, L))**2]
+    mag_values = [magnetization(spins, L)]
+    mag_values_squared = [(magnetization(spins, L))**2]
     for step in range(steps):
         spins = metropolis_algorithim(spins, beta, K, h, L)
-        if step % (steps//100) == 0:
-            energies.append(calculate_energy(spins, K, h, L))
-    return spins, energies
+        if step % 100 == 0:
+            energy = calculate_energy(spins, K, h, L)
+            energies.append(energy)
+            energies_squared.append(energy**2)
+            mag_value = magnetization(spins, L)
+            mag_values.append(mag_value)
+            mag_values_squared.append(mag_value**2)
+    return spins, energies, energies_squared, mag_values, mag_values_squared
 
 def magnetization(spins, L):
     net_spin = 0
@@ -58,16 +67,27 @@ def magnetization(spins, L):
             net_spin += spins[i, j]
     return net_spin/(L**2)
 
+def heat_capacity(energy_value, energy_squared, beta):
+    average_energy = np.average(energy_value)
+    average_energy_squared = np.average(energy_squared)
+    return (beta**2) *(average_energy_squared-(average_energy**2))
+
 
 def run_monte_carlo(beta, K, h, L, steps, spins):
     magnetization_values = []
+    heat_capacity_values = []
 
     for beta_value in beta:
         copy_of_spins = spins.copy()
-        final_spins, energies = monte_carlo(beta_value, K, h, L, steps, copy_of_spins)
+        final_spins, energy_accumulator, energy_squared, magnetization_acumulator, magnetization_squared  = monte_carlo(beta_value, K, h, L, steps, copy_of_spins)
+
         magnetization_value = magnetization(final_spins, L)
         magnetization_values.append(magnetization_value)
-    return magnetization_values
+
+        heat_capacity_value = heat_capacity(energy_accumulator, energy_squared, beta_value)
+        heat_capacity_values.append(heat_capacity_value)
+
+    return magnetization_values, heat_capacity_values
 
 def Visualization_of_MC(initial_spins, final_spins, energies):
     plt.figure(figsize=(12, 5))
@@ -98,23 +118,32 @@ def Visualization_of_MC(initial_spins, final_spins, energies):
     plt.show()
 
 
-temp = np.linspace(0.001, 3, 500)
+temp = np.linspace(0.0001, 5, 100)
 beta = []
 for temp_value in temp:
     beta.append(1/temp_value)
 
 K = 1
 h = 0
-L = 50
-steps = 10000
+L = 25
+#beta = 10000
+steps = 100000
 
 spins_initial = initial_spins(L)
 copy_of_spins_initial = spins_initial.copy()
+#final_spins, energy = monte_carlo(beta, K, h, L, steps, copy_of_spins_initial)
+#Visualization_of_MC(spins_initial, final_spins, energy)
 
-magnetization_values = run_monte_carlo(beta, K, h, L, steps, copy_of_spins_initial)
+magnetization_values, heat_capacity_values = run_monte_carlo(beta, K, h, L, steps, copy_of_spins_initial)
 
-plt.plot(temp, magnetization_values, label="Monte Carlo Steps")
+plt.plot(temp, np.abs(magnetization_values), label="Monte Carlo Steps")
+plt.ylabel("Magnetization")
+plt.xlabel("Temperature")
 plt.show()
 
-
+plt.plot(temp, heat_capacity_values, label="Monte Carlo Steps")
+plt.semilogy()
+plt.ylabel("Heat Capacity")
+plt.xlabel("Temperature")
+plt.show()
 
